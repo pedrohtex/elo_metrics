cf8726e425c15825cc91dc6c230906a58db9f062
Pedro Henrique Teixeira <Pedro Henrique Teixeira>
2016-05-21 15:36:46 -0300

    First Scrape test

---
+++ scrape/server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+
+url = 'http://www.resultadoonline.com/masterAtletaMas.asp';
+
+request(url, function(error, response, html){
+    if(!error){
+        var $ = cheerio.load(html);
+
+    var title, release, rating;
+    var json = { title : "", release : "", rating : ""};
+        
+    $('table[border=1] tr').each(function (i, row) {
+        var _row = $(row);
+        console.log(i);
+        _row.children('td').each(function (k, cell) {
+            var _cell = $(cell);
+            console.log(_cell.text());
+        });
+        
+    })
+
+    $('h1[itemprop=name]').filter(function(){
+        var data = $(this);
+        title = data.text();            
+        release = data.children().last().children().text();
+
+        json.title = title;
+        json.release = release;
+    })
+
+    $('span[itemprop=ratingValue]').filter(function(){
+        var data = $(this);
+        rating = data.text();
+
+        json.rating = rating;
+    })
+}
+
+// To write to the system we will use the built in 'fs' library.
+// In this example we will pass 3 parameters to the writeFile function
+// Parameter 1 :  output.json - this is what the created filename will be called
+// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+// Parameter 3 :  callback function - a callback function to let us know the status of our function
+
+fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+    console.log('File successfully written! - Check your project directory for the output.json file');
+
+})
+
+// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+res.send('Check your console!')
+
+    }) ;
+})
+
+app.listen('8081')
+
+console.log('Magic happens on port 8081');
+
+exports = module.exports = app;
